97d39046bf5d6bb13e8799b4421579ba707ee973
jackzhu <ulovecode@gmail.com>
2019-05-16 02:42:01 +0800

    【新增】添加简单的构建

--- build.sh
+++ build.sh
@@ -1,22 +1,6 @@
-#!/bin/bash
-images=(
-    kube-apiserver-amd64:v1.11.2
-    kube-controller-manager-amd64:v1.11.2
-    kube-scheduler-amd64:v1.11.2
-    kube-proxy-amd64:v1.11.2
-    pause:3.1
-    etcd-amd64:3.2.18
-    coredns:1.1.3
 
-    pause-amd64:3.1
+pip install -r requirements.txt
 
-    kubernetes-dashboard-amd64:v1.10.0
-    heapster-amd64:v1.5.4
-    heapster-grafana-amd64:v5.0.4
-    heapster-influxdb-amd64:v1.5.2
-)
+cp -rfp inventory/sample inventory/mycluster
 
-for imageName in ${images[@]} ; do
-    docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName
-    docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName k8s.gcr.io/$imageName
-done
+ansible-playbook -i inventory/mycluster/hosts.ini cluster.yml -b -v -k
