71ca7044fe0296794ca4ac30b76aa70be17c582c
jackzhu <ulovecode@gmail.com>
2019-05-16 03:13:24 +0800

    【修改】添加资源

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@ pip install -r requirements.txt
 
 cp -rfp inventory/sample inventory/mycluster
 
+tar -cvf resources/kubernetes-client-linux-amd64.tar.gz /tmp/releases/kubeadm
+
 ansible-playbook -i inventory/mycluster/hosts.ini cluster.yml -b -v -k
