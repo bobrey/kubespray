c9651c772c048afe936d068ac7cb342c9baa4d97
jackzhu <ulovecode@gmail.com>
2019-05-16 03:38:48 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ pip install -r requirements.txt
 
 cp -rfp inventory/sample inventory/mycluster
 
-tar -cvf resources/kubernetes-client-linux-amd64.tar.gz /tmp/releases/kubeadm
+mkdir /tmp/releases/kubeadm
+
+cp ./resources/kubectl  /tmp/releases/kubeadm/
 
 ansible-playbook -i inventory/mycluster/hosts.ini cluster.yml -b -v -k
